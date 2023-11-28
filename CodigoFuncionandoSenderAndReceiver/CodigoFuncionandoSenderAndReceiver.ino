be313479feea0205143a6a62bbc76bf8db0b849f
Pedro Lucas Bezerra Mendes <104802373+PedroLucasMendes@users.noreply.github.com>
2023-11-28 18:37:37 -0400

    Add files via upload

--- CodigoFuncionandoSenderAndReceiver/CodigoFuncionandoSenderAndReceiver.ino
+++ CodigoFuncionandoSenderAndReceiver/CodigoFuncionandoSenderAndReceiver.ino
@@ -5,11 +5,6 @@ SX1276 radio = new Module(18, 26, 14, 35);
 unsigned long currentMillis;
 unsigned long startMillis = 0;
 
-//Vetores Spreading Factor, BandWidth and Transmission Power
-int Vetor_SF[] = {7,8,9,10,11,12}; //6
-float Vetor_BW[] = {125,250,500}; //3
-int Vetor_PT[] = {10,11,12,13,14,15,16,17,18,19,20}; //11
-
 
 bool startsWith(const String &str, const String &prefix) {
     return str.substring(0, prefix.length()) == prefix;
@@ -22,7 +17,7 @@ void SetDefaultParam(){
   radio.setBandwidth(125.0);
   radio.setSpreadingFactor(12);
   radio.setOutputPower(20);
-  Serial.println("Parametros default setados");
+  //Serial.println("Parametros default setados");
   
 }
 
@@ -30,11 +25,9 @@ void changeParam(int SF, float BW, int PT){
   
   //Troca de parametros
   radio.setBandwidth(BW);
-  Serial.println("Trocou para o BandWidth "+String(BW));
   radio.setSpreadingFactor(SF);
-  Serial.println("Trocou para o Spreading Factor "+String(SF));
   radio.setOutputPower(PT);
-  Serial.println("Trocou para o Transmission Power "+String(PT));
+  Serial.println("TRC:SF"+String(SF)+"/BW"+String(BW)+"/PT"+String(PT));
 }
 
 void Receiver_HandShake(){
@@ -52,7 +45,7 @@ void Receiver_HandShake(){
   while(true){
     state = radio.receive(str_read);
     if(state == RADIOLIB_ERR_NONE && str_read.equals(msg_sync)){
-      Serial.println(F("[SX1262] SYNC_HANDR Received!"));
+      //Serial.println(F("SYNC_HANDR Received!"));
       break;
     }
   }
@@ -60,19 +53,20 @@ void Receiver_HandShake(){
   while(loop){
     
     state = radio.transmit("SYNC+ACK_HAND");
+    //Serial.println("Enviou SYNC+ACK_HAND");
     while(currentMillis - startMillis <= period) {
       currentMillis = millis();  
       String ack_hand = "ACK_HAND";
       state = radio.receive(str_read);
       if(state == RADIOLIB_ERR_NONE && str_read.equals(ack_hand)){
-          Serial.println(F("[SX1262] ACK_HAND Received!"));
+          //Serial.println(F("[SX1262] ACK_HAND Received!"));
           loop = false;
           break;
       }
       
     }
   }
-  
+  Serial.println("Terminou HandShake");
 }
 
 void Receiver_SendPacket(){
@@ -88,36 +82,37 @@ void Receiver_SendPacket(){
   startMillis = currentMillis;
   while(loop){
     currentMillis = millis();
-    Serial.println("Esperando mensagens");
+    //Serial.println("Esperando mensagens");
 
     int state = radio.receive(str_read);
     if(state == RADIOLIB_ERR_NONE && str_read.equals(str_finish)){
-      Serial.print("Mensagem recebida: ");
-      Serial.println(str_read);
+      //Serial.print("Mensagem recebida: ");
+      //Serial.println(str_read);
       startMillis = currentMillis;  
       loop = false;
     }
     else if(state == RADIOLIB_ERR_NONE){
       
-      Serial.print("Mensagem recebida: ");
-      Serial.println(str_read);
+      //Serial.print("Mensagem recebida: ");
+      //Serial.println(str_read);
+      Serial.print("MSGR");
+      Serial.print(";"+String(radio.getRSSI()));
+      Serial.println(";"+String(radio.getSNR()));
       delay(2000);
       state = radio.transmit("ACK_PACKET");
       //radio.finishTransmit();
-      Serial.println("Msg ENVIADA");
+      //Serial.println("Msg ENVIADA");
       radio.startReceive(); 
       startMillis = currentMillis;  
       
     }
-    else{
-      Serial.println(state);
-    }
     if(currentMillis - startMillis >= period){
        startMillis = currentMillis;   
-       Serial.println("Sem comunicação na troca");
+       Serial.println("SEMPACTRC");
        loop = false;
     }    
   }
+  Serial.println("Terminou recebimento de pacote");
 }
 
 
@@ -126,18 +121,18 @@ void setup() {
   Serial.begin(115200);
   while(!Serial);
 
-  Serial.print(F("[SX1262] Initializing ... "));
+  //Serial.print(F("[SX1262] Initializing ... "));
   int state = radio.begin();
   if (state == RADIOLIB_ERR_NONE) {
-    Serial.println(F("success!"));
+    //Serial.println(F("success!"));
   } else {
-    Serial.print(F("failed, code "));
-    Serial.println(state);
+    //Serial.print(F("failed, code "));
+    //Serial.println(state);
     while (true);
   }
   
   if (radio.setFrequency(915.0) == RADIOLIB_ERR_INVALID_FREQUENCY) {
-    Serial.println(F("Selected frequency is invalid for this module!"));
+    //Serial.println(F("Selected frequency is invalid for this module!"));
     while (true);
   }
 
@@ -151,18 +146,19 @@ void loop() {
   String F = "F";
 
   String str_read;
-
+  Serial.println("Começando recebimento da mensagem de troca");
   SetDefaultParam();
   while(true){
     int state = radio.receive(str_read);
-    Serial.println("Esperando mensagem para troca de parametro");
+    //Serial.println("Esperando mensagem para troca de parametro");
     if(state == RADIOLIB_ERR_NONE){
-      Serial.print("Mensagem recebida: ");
-      Serial.println(str_read);
+      //Serial.print("Mensagem recebida: ");
+      //Serial.println(str_read);
       state = radio.transmit("TROCA_ACK");
       break;
     }
   }
+  Serial.println("Recebido mensagem de troca");
 
     String valores[3];
     int cont = 0;
--- CodigoFuncionandoSenderAndReceiver/Send_WoWMoM.ino
+++ CodigoFuncionandoSenderAndReceiver/Send_WoWMoM.ino
@@ -14,9 +14,9 @@ unsigned long currentMillis;
 
 
 //Vetores Spreading Factor, BandWidth and Transmission Power
-int Vetor_SF[] = {12,11,10,9,8,7}; //6
+int Vetor_SF[] = {7,8,9,10,11,12}; //6
 float Vetor_BW[] = {125,250,500}; //3
-int Vetor_PT[] = {10,12,14,16,18,20}; //11
+int Vetor_PT[] = {10,12,14,16,18,20}; //6
 
 void SetDefaultParam(){
   
@@ -24,7 +24,7 @@ void SetDefaultParam(){
   radio.setBandwidth(125.0);
   radio.setSpreadingFactor(12);
   radio.setOutputPower(20);
-  Serial.println("Parametros default setados");
+  //Serial.println("Parametros default setados");
   
 }
 
@@ -32,11 +32,9 @@ void changeParam(int SF, float BW, int PT){
   
   //Troca de parametros
   radio.setBandwidth(BW);
-  Serial.println("Trocou para o BandWidth "+String(BW));
   radio.setSpreadingFactor(SF);
-  Serial.println("Trocou para o Spreading Factor "+String(SF));
   radio.setOutputPower(PT);
-  Serial.println("Trocou para o Transmission Power "+String(PT));
+  Serial.println("TRC:SF"+String(SF)+"/BW"+String(BW)+"/PT"+String(PT));
 }
 
 
@@ -51,24 +49,24 @@ void Send_HandShake(){
   String msg_read;
   bool loop = true;
 
-  Serial.println("Começando HandShake");
+  Serial.println("Comecando HandShake");
   SetDefaultParam();
  
   while(loop){
     state = radio.transmit(msg_sync);
     radio.startReceive();
-    Serial.println("Enviou SYNC");
+    //Serial.println("Enviou SYNC");
     
     while(currentMillis - startMillis <= period){
       currentMillis = millis();
       state = radio.receive(msg_read);
-      Serial.println("Recebendo msg");
+      //Serial.println("Recebendo msg");
       if(state == RADIOLIB_ERR_NONE && msg_read.equals(msg_sync_ack)){
-         Serial.print("Mensagem recebida: ");
-         Serial.println(msg_read);
+         //Serial.print("Mensagem recebida: ");
+         //Serial.println(msg_read);
          state = radio.transmit(msg_ack);
          radio.startReceive();
-         Serial.println("Enviou ACK");
+         //Serial.println("Enviou ACK");
          startMillis = currentMillis;
          loop = false;
          break;
@@ -76,6 +74,7 @@ void Send_HandShake(){
     }
     startMillis = currentMillis;
   }
+  Serial.println("Terminou HandShake");
   
 }
 
@@ -88,47 +87,42 @@ void SendPacket(){
   String msg_read;
   int i;
 
-  Serial.println("Começando o envio de pacotes");
-  for(i = 1; i <= 50; i++){
+  Serial.println("Começando recebimento de pacote");
+  for(i = 1; i <= 100; i++){
     String string_envio = packet+String(i);
     int state = radio.transmit(string_envio);
     radio.startReceive();
-    Serial.println("Enviou "+string_envio);
+    Serial.println(string_envio);
     startMillis = currentMillis;
     while(currentMillis - startMillis <= period){
        currentMillis = millis();
        state = radio.receive(msg_read);
-       Serial.println("Recebendo msg");
        if(state == RADIOLIB_ERR_NONE && msg_read.equals(msg_ack_packet)){
-           Serial.print("Mensagem recebida: ");
-           Serial.println(msg_read);
+           Serial.print("MSGR");
+           //Serial.print(msg_read);
+           Serial.print(";"+String(radio.getRSSI()));
+           Serial.println(";"+String(radio.getSNR()));
            break;
-       } else if (state == RADIOLIB_ERR_CRC_MISMATCH) {
-          // packet was received, but is malformed
-          Serial.println(F("CRC error!"));
-    
-        } else {
-          // some other error occurred
-          Serial.print(F("failed, code "));
-          Serial.println(state);
-    
-        }
+       }
     }
     //Verificador se perdeu pacote
     if(currentMillis - startMillis >= period){
-      Serial.println("Perdeu o pacote");
+      Serial.print("PDR");
+      Serial.print(";"+String(radio.getRSSI()));
+      Serial.println(";"+String(radio.getSNR()));
       cont++;
     }
     startMillis = currentMillis;
     //Verificação se perdeu 5 pacotes
-    if(cont >= 5){
-      Serial.println("Perdeu 5 pacotes, vai para a proxima troca");
-      i = 52;
+    if(cont >= 50){
+      Serial.println("PDR 50");
+      i = 102;
     }
   }
-  if(i == 51){
+  if(i == 101){
     String string_termino = "TERM";
     radio.transmit(string_termino);
+    Serial.println("Terminou recebimento de pacote");
   }
 }
 
@@ -138,17 +132,17 @@ void setup() {
   while(!Serial);
   
   // initialize SX1262 with default settings
-  Serial.print(F("Initializing ... "));
+  //Serial.print(F("Initializing ... "));
   int state = radio.begin();
   if (state == RADIOLIB_ERR_NONE) {
-    Serial.println(F("success!"));
+    //Serial.println(F("success!"));
   } else {
-    Serial.print(F("failed, code "));
-    Serial.println(state);
+    //Serial.print(F("failed, code "));
+    //Serial.println(state);
     while (true);
   }
   if (radio.setFrequency(915.0) == RADIOLIB_ERR_INVALID_FREQUENCY) {
-    Serial.println(F("Selected frequency is invalid for this module!"));
+    //Serial.println(F("Selected frequency is invalid for this module!"));
     while (true);
   }
 
@@ -166,7 +160,7 @@ void loop() {
   String msg_troca_ack = "TROCA_ACK";
   String msg_read;
 
-  Serial.println("Começando a troca de parametros");
+  Serial.println("Começando recebimento da mensagem de troca");
   for(int SF = 0; SF < 6; SF++){
     for(int BW = 0; BW < 3; BW++){
       for(int PT = 0; PT < 11; PT++){
@@ -174,23 +168,19 @@ void loop() {
         SetDefaultParam();
         //7/125.0/12
         String string_troca = String(Vetor_SF[SF])+"/"+String(Vetor_BW[BW])+"/"+String(Vetor_PT[PT])+"/";
-        Serial.println("Os parametros de troca são:");
-        Serial.println("Spreading Factor: "+String(Vetor_SF[SF]));
-        Serial.println("BandWidth: "+String(Vetor_BW[BW]));
-        Serial.println("Transmission Power: "+String(Vetor_PT[PT]));
         
         while(loop){
           int state = radio.transmit(string_troca);
-          Serial.println("Enviou "+string_troca);
+          //Serial.println("Enviou "+string_troca);
           
           startMillis = currentMillis;
           while(currentMillis - startMillis <= period){
             currentMillis = millis();
             state = radio.receive(msg_read);
-            Serial.println("Recebendo msg");
+            //Serial.println("Recebendo msg");
             if(state == RADIOLIB_ERR_NONE && msg_read.equals(msg_troca_ack)){
-               Serial.print("Mensagem recebida: ");
-               Serial.println(msg_read);
+               //Serial.print("Mensagem recebida: ");
+               //Serial.println(msg_read);
                
                changeParam(Vetor_SF[SF], Vetor_BW[BW], Vetor_PT[PT]);
                SendPacket();
@@ -206,4 +196,7 @@ void loop() {
       }
     }
   }
+
+  Serial.println("Experimentos terminou");
+  while(true){}
 }
