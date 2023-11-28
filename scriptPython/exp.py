bfeb6a57129045b5f0a49a841902da0a6745476d
Mateus Mota <83120884+Mateuxx@users.noreply.github.com>
2023-11-28 16:45:19 -0400

    Add files via upload

---
+++ scriptPython/exp.py
@@ -0,0 +1,33 @@
+import serial
+import time
+import os
+import logging
+
+porta_serial = '/dev/ttyUSB0'
+baudrate = 115200
+timeout = 1
+
+
+logger=logging.getLogger('log')
+logging.basicConfig(filename='/home/pi/Desktop/send_experimento.log',
+        filemode='a',
+        format='%(asctime)s,%(msecs)d;%(name)s;%(levelname)s;%(message)s',
+        datefmt='%H:%M:%S',
+        level=logging.INFO)
+
+def write_read(arduino):
+    while True:
+        data = arduino.readline().decode()
+        if data:
+            print(data)
+            logging.info(data + '\n')
+
+def main():
+    arduino = serial.Serial(port=porta_serial, baudrate=baudrate, timeout=0.5)
+    print(f"Gravando dados")
+
+    write_read(arduino)
+    arduino.close()
+
+if __name__ == '__main__':
+    main()
