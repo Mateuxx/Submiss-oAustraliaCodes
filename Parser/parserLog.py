f0b79f1801d55156848da4f8a98453f24bfce758
Pedro Lucas Bezerra Mendes <104802373+PedroLucasMendes@users.noreply.github.com>
2023-12-05 16:01:05 -0400

    Add files via upload

--- Parser/parserLog.py
+++ Parser/parserLog.py
@@ -1,9 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
-
- 
+import seaborn as sns
+from matplotlib import cm
+import numpy as np
 vetor_dicio_info = []
+
+def plot_correlation(data):
+    '''
+    plot the correlation's matrix to explore dependency between features
+    '''
+    fig = plt.figure(figsize=(8,6), dpi=300)
+    sns.heatmap(data.corr(), annot=True, fmt=".2f", cmap=cm.coolwarm_r)
+    plt.show()
+    #fig.savefig('corr.png')
 
 
 def pre_processamento():
@@ -19,34 +28,54 @@ def pre_processamento():
             arquivo_modificado.write(dado)
         
 def readArquivo():
+    cont = 0
     with open('sendLog.txt', 'r') as arquivo:
         linhas = arquivo.readlines()
         for linha in linhas:    
-            linha = linha.split(";")
+            dados = linha.split(";")
+            
             dicio_info = {}
-            dicio_info["Hora"] = linha[0]
-            dicio_info["SF"] = linha[1]
-            dicio_info["BW"] = linha[2]
-            dicio_info["PT"] = linha[3]
-            dicio_info["RSSI"] = linha[4]
-            dicio_info["SNR"] = linha[5].replace("\n","")
-            vetor_dicio_info.append(dicio_info)
+            if(dados[1] != "SEMPACTRC\n"):
+                dicio_info["Hora"] = dados[0]
+                dicio_info["SF"] = dados[1]
+                dicio_info["BW"] = dados[2]
+                dicio_info["PT"] = dados[3]
+                dicio_info["RSSI"] = dados[4]
+                dicio_info["SNR"] = dados[5].replace("\n","")
+                vetor_dicio_info.append(dicio_info)
+            else:
+                cont+=1
+    print("Perdas ao total: ",cont)
     df = pd.DataFrame.from_dict(vetor_dicio_info)
+    for linha in df:
+        print(linha+"OIEE")
     df.to_csv('logWoWMoM.csv', index=False)
-    print(df)
-    eixo_x = df.iloc[:,1]
-    eixo_y = df.iloc[:,4]
-    plt.scatter(eixo_x, eixo_y)
-    plt.xlabel('Eixo X')
-    plt.ylabel('Eixo Y')
-    plt.title('Gráfico de Dispersão')
-    plt.show()   
+    '''
+    df['PT'] = pd.to_numeric(df['PT'], errors='coerce')
+    df['RSSI'] = pd.to_numeric(df['RSSI'], errors='coerce')
+    df['SF'] = pd.to_numeric(df['SF'], errors='coerce')
+    df['BW'] = pd.to_numeric(df['BW'], errors='coerce')
+    fig, axs = plt.subplots(2, 2, figsize=(10, 10))
+
+    sns.boxplot(x='SF', y='RSSI', data=df[['SF', 'RSSI']],
+                notch=False, linewidth=1.5, palette = "Set2", width=0.25,
+                medianprops={"color": "black"}, ax=axs[0, 0])
+    sns.boxplot(x='BW', y='RSSI', data=df[['BW', 'RSSI']],
+                notch=False, linewidth=1.5, palette = "Set2", width=0.25,
+                medianprops={"color": "black"}, ax=axs[0, 1])
+    sns.boxplot(x='PT', y='RSSI', data=df[['PT', 'RSSI']],
+                notch=False, linewidth=1.5, palette = "Set2", width=0.25,
+                medianprops={"color": "black"}, ax=axs[1, 0])
+    axs[1, 1].axis('off')
+    plt.tight_layout()
+    plt.show()
+
+    '''
 
 
-def main():
-    # Chamar a função para realizar a operação
-    pre_processamento()
-    readArquivo()
 
-if __name__ == '__main__':
-    main()
+
+pre_processamento()
+readArquivo()
+   
+
